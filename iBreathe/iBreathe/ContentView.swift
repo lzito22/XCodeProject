41220039d3f8638d92ceb4e2711100ffe461c43f
lzito22 <113601247+lzito22@users.noreply.github.com>
2022-11-21 10:03:26 +0100

    Add files via upload

---
+++ iBreathe/iBreathe/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iBreathe
+//
+//  Created by Luigi Zito on 21/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iBreathe/iBreathe/iBreatheApp.swift
@@ -0,0 +1,17 @@
+//
+//  iBreatheApp.swift
+//  iBreathe
+//
+//  Created by Luigi Zito on 21/11/22.
+//
+
+import SwiftUI
+
+@main
+struct iBreatheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
